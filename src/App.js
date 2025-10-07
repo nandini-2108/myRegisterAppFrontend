11fb90aa11971caacc562100dbdb912e5a7a025f
nandini-2108 <alapatinandini93@gmail.com>
2025-10-07 12:31:58 +0530

    Frontend initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './App.css';
+
+import axios from 'axios';
+
+function App() {
+  const [form, setForm] = useState({ name: '', email: '', password: '' });
+
+  const handleChange = e => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/register', form);
+      alert('Registered successfully!');
+      setForm({ name: '', email: '', password: '' });
+    } catch (err) {
+      alert('Error registering user');
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: '400px', margin: '50px auto' }}>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <input name="name" placeholder="Name" value={form.name} onChange={handleChange} required /><br /><br />
+        <input name="email" placeholder="Email" value={form.email} onChange={handleChange} required /><br /><br />
+        <input name="password" placeholder="Password" type="password" value={form.password} onChange={handleChange} required /><br /><br />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
